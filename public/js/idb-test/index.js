f734ce008d417e9fa1cde0024b0f2ba40f1d4aa8
Koko Dev <koko.webdev@gmail.com>
2018-07-10 11:43:28 -0400

    In idb-test, add openCursor to go through one item at a time in the people store using 'age' index and and ordered by the 'age' property

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -93,7 +93,8 @@ dbPromise.then(db => {
   console.log('People added');
 });
 
-// To read the people in the people store
+
+// TODO: read the people in the people store
 dbPromise.then(db => {
   let tx = db.transaction('people');
   let peopleStore = tx.objectStore('people');
@@ -117,6 +118,7 @@ dbPromise.then(db => {
   console.log('People:', people);
 });
 
+
 // TODO: console log all people ordered by age
 dbPromise.then(db => {
   let tx = db.transaction('people');
@@ -129,3 +131,39 @@ dbPromise.then(db => {
 }).catch(err => {
   console.log('Error sorting People by age:', err);
 });
+
+
+// TODO: console log all people ordered by age
+dbPromise.then(db => {
+  let tx = db.transaction('people');
+  let peopleStore = tx.objectStore('people');
+  let ageIndex = peopleStore.index('age');
+  
+  // Get all of the people sorted by age  (and then log people to the console)
+  // return ageIndex.getAll();
+  
+  // Using Cursors, we can go through one item at a time rather than getting all of the people out of the store
+  // Rather than calling getAll(), open a Cursor
+  // This returns a Promise for a Cursor Object representing the first item in the index,
+  //      or undefined if there isn't one
+  return ageIndex.openCursor();
+}).then(function logPerson(cursor) {
+  // If it is undefined, just return
+  if(!cursor) return;
+  
+  // Otherwise, log it
+  // The first person in the index is in cursor.value
+  console.log('Cursored at:', cursor.value.name);
+  
+  // Then call cursor.continue() to move on to the next item
+  // cursor.continue() returns a Promise for a cursor representing the next item
+  //        or it returns undefined if there isn't one
+  // Then, Call function logPerson once cursor.continue resolves
+  // This sets up an asynchronous loop until cursor is undefined  (end of list)
+  return cursor.continue().then(logPerson);
+  }).then(() => {
+    console.log(('Done cursoring'));
+  });
+
+
+
