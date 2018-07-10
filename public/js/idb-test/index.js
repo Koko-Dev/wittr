abb4bd4e70a5ac5e2f0617c4c340e89df724f22a
Koko Dev <koko.webdev@gmail.com>
2018-07-10 09:47:22 -0400

    Populate indexedDB people store with data
    Because keyPath is 'name', the put property only requires a value (the people data)

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -41,5 +41,44 @@ dbPromise.then(db => {
   return tx.complete;
 }).then(function() {
   console.log('Added favoriteAnimal: dog');
-  
 });
+
+dbPromise.then(db => {
+  let tx = db.transaction('people', 'readwrite');
+  let peopleStore = tx.objectStore('people');
+  
+  // because keyPath is 'name' is the key, we only need the value
+  peopleStore.put({
+    name: 'Sam Munoz',
+    age: 25,
+    favoriteAnimal: 'cat'
+  });
+  
+  peopleStore.put({
+    name: 'DK Davic',
+    age: 26,
+    favoriteAnimal: 'cat'
+  });
+  
+  peopleStore.put({
+    name: 'JK Jordan',
+    age: 27,
+    favoriteAnimal: 'lizard'
+  });
+  
+  peopleStore.put({
+    name: 'CK Chelsey',
+    age: 35,
+    favoriteAnimal: 'koala'
+  });
+  
+  peopleStore.put({
+    name: 'BK Bree',
+    age: 15,
+    favoriteAnimal: 'dog'
+  });
+  
+  return tx.complete;
+}).then(() => {
+  console.log('People added');
+})
